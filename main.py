70d940b6e64aafd0b62f1bc0a6b3c48387d94114
Polykek2K <45636036+Polykek2K@users.noreply.github.com>
2022-12-16 05:04:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ db = client.gas_vybory
 
 def goThroughUiks(browser, uik, json_candidates):
     if len(browser.find_elements(by=By.XPATH, value=uik + '/ul/li/a')) == 0:
+        recent_cands = []
         candidates = {_['candidate_id']: _['name'] for _ in json_candidates}
         table = browser.find_element(by=By.CLASS_NAME, value='table-bordered')
         if table:
@@ -50,17 +51,19 @@ def goThroughUiks(browser, uik, json_candidates):
             for i, cand in enumerate(raw_candidates):
                 for p_id, p_name in candidates.items():
                     if cand[0] == p_name:
+                        temp = next(item for item in json_candidates if item["name"] == p_name)
+                        recent_cands.append(temp)
                         current_json_results["candidates_results"][i] = {'candidate_id': p_id, 'result': int(cand[1])}
                         break
             if db.results.find_one(current_json_results.copy()) is not None:
                 return
             post_id = db.results.insert_one(current_json_results.copy())
 
-            for candidate in json_candidates:
+            for candidate in recent_cands:
                 candidate['oik_id'] = getOik(browser)
-                if len(list(db.candidates.find({"candidate_id": candidate["candidate_id"]}))) == 1:
-                    continue
-                rec_id = db.candidates.insert_one(candidate.copy())
+                if len(list(db.candidates.find({"candidate_id": candidate["candidate_id"]}))) > 0:
+                     continue
+                post_id = db.candidates.insert_one(candidate.copy())
 
     else:
         browser.find_elements(by=By.XPATH, value=uik)[0].click()
@@ -226,32 +229,49 @@ def parseTableByXPATH(browser, xpath, type='results', table_format="221", jsn=No
     return parseTable(browser, table, type, table_format, jsn)
 
 
+def extendCandidates(browser, link=None):
+    current_json_candidates = []
+    tableArr = []
+    if link is not None:
+        browser.get(link)
+    tables = ['//*[@id="candidates-221-2"]/tbody', '//*[@id="candidates-220-2"]/tbody']
+    table = browser.find_elements(by=By.XPATH, value='//*[@id="candidates-221-2"]/tbody/tr/td/a')
+    if len(table) <= 0:
+        table = browser.find_elements(by=By.XPATH, value='//*[@id="candidates-221-2"]/tbody/tr/td/nobr/a')
+    table2 = browser.find_elements(by=By.XPATH, value='//*[@id="candidates-220-2"]/tbody/tr/td/a')
+    if len(table2) <= 0:
+        table2 = browser.find_elements(by=By.XPATH, value='//*[@id="candidates-220-2"]/tbody/tr/td/nobr/a')
+    bln = len(browser.find_elements(by=By.XPATH, value=tables[0])) > 0
+    if len(table) <= 0 and len(table2) <= 0:
+        return -1, -1
+    temp = list(parseTableByXPATH(browser, tables[0] if bln else tables[1],
+                                  type="candidates", table_format=("221" if bln else "220")))
+    current_json_candidates.extend(temp)
+    actual_table = table if len(table) > 0 else table2
+    for _ in actual_table:
+        tableArr.append(_.get_attribute('href'))
+    return current_json_candidates, tableArr
+
+
 def parseCandidates(browser):
     tableArr = []
     listsArr = []
     current_json_candidates = []
     browser.find_element(by=By.XPATH, value="/html/body/div[2]/main/div[2]/div[2]/div[1]/ul/li/a[2]").click()
     if len(browser.find_elements(by=By.XPATH, value='//*[@id="report-body col"]/div[10]/div/div[5]/ul[1]/li/a')) > 0:
+        tempCandidates, tempTable = extendCandidates(browser)
+        if tempCandidates == -1 and tempTable == -1:
+            return "continue"
+        tableArr.extend(tempTable)
+        current_json_candidates.extend(tempCandidates)
         for _ in browser.find_elements(by=By.XPATH, value='//*[@id="report-body col"]/div[10]/div/div[5]/ul[1]/li/a'):
             listsArr.append(_.get_attribute('href'))
         for _ in listsArr:
-            browser.get(_)
-            tables = ['//*[@id="candidates-221-2"]/tbody', '//*[@id="candidates-220-2"]/tbody']
-            table = browser.find_elements(by=By.XPATH, value='//*[@id="candidates-221-2"]/tbody/tr/td/a')
-            if len(table) <= 0:
-                table = browser.find_elements(by=By.XPATH, value='//*[@id="candidates-221-2"]/tbody/tr/td/nobr/a')
-            table2 = browser.find_elements(by=By.XPATH, value='//*[@id="candidates-220-2"]/tbody/tr/td/a')
-            if len(table2) <= 0:
-                table2 = browser.find_elements(by=By.XPATH, value='//*[@id="candidates-220-2"]/tbody/tr/td/nobr/a')
-            bln = len(browser.find_elements(by=By.XPATH, value=tables[0])) > 0
-            if len(table) <= 0 and len(table2) <= 0:
+            tempCandidates, tempTable = extendCandidates(browser, _)
+            if tempCandidates == -1 and tempTable == -1:
                 return "continue"
-            temp = parseTableByXPATH(browser, tables[0] if bln else tables[1],
-                                     type="candidates", table_format=("221" if bln else "220"))
-            current_json_candidates.extend(temp)
-            actual_table = table if len(table) > 0 else table2
-            for _ in actual_table:
-                tableArr.append(_.get_attribute('href'))
+            tableArr.extend(tempTable)
+            current_json_candidates.extend(tempCandidates)
         for i in range(len(tableArr)):
             browser.get(tableArr[i])
             solveCaptcha(browser)
@@ -357,7 +377,7 @@ def observeData(browser):
         current_json_vrn["title"] = str(
             browser.find_element(by=By.XPATH, value='//*[@id="election-title"]').text.split('\n')[0])
         current_json_vrn["level"] = envir["level"]
-        current_json_vrn["date"] = date_of_vote
+        current_json_vrn["date"] = datetime.strptime(date_of_vote, '%d.%m.%Y').date()
         if db.elections.find_one(current_json_vrn.copy()) is not None:
             continue
         post_id = db.elections.insert_one(current_json_vrn.copy())
